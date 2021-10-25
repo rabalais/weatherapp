6cbfba57201ab1f4f180184b0ca27ea74047539c
Noah Rabalais <60150831+rabalais@users.noreply.github.com>
2021-10-24 20:20:35 -0500

    update

--- get-weather.js
+++ get-weather.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
 
-    $.getJSON("api.openweathermap.org/data/2.5/weather?q=New York&appid=ad0b6203c995478faefc4d58318768ca", function(data, status){
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=New York&appid=ad0b6203c995478faefc4d58318768ca", function(data, status){
       console.log("Data: " + data + "Status: " + status);
-    });
-
-    $("#search-button").click(function(){
-      var searchInput = $("#location-search-box").val();
-
     });
 
     $("#location-search-box").click(function(){
