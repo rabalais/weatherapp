3a6dc9a25aa46824abd0c91cd9d34348fb168410
Noah Rabalais <60150831+rabalais@users.noreply.github.com>
2021-10-24 20:40:49 -0500

    imperial units

--- get-weather.js
+++ get-weather.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=New York&appid=ad0b6203c995478faefc4d58318768ca", function(data, status){
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=New York&units=imperial&appid=ad0b6203c995478faefc4d58318768ca", function(data, status){
       console.log("Data: " + data + "Status: " + status);
       formatData(data);
     });
