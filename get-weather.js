90eeff524bf4baf0f72ea79d6a328cdb5ed2eff3
Noah Rabalais <60150831+rabalais@users.noreply.github.com>
2021-10-24 20:39:04 -0500

    formatting data

--- get-weather.js
+++ get-weather.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=New York&appid=ad0b6203c995478faefc4d58318768ca", function(data, status){
       console.log("Data: " + data + "Status: " + status);
+      formatData(data);
     });
 
     $("#location-search-box").click(function(){
@@ -9,11 +10,33 @@ $(document).ready(function(){
     });
 });
 
-function updateWeather(data) {
-  
-}
 
 function getWeatherInfo() {
 
 }
 
+function formatData(jsonObj) {
+  var city_name = jsonObj.name;
+  var city_weather = jsonObj.weather[0].main;
+  var city_weather_desc = jsonObj.weather[0].description;
+  var city_temp = jsonObj.main.temp;
+  var city_feels = jsonObj.main.feels_like;
+  var city_temp_min = jsonObj.main.temp_min;
+  var city_temp_max = jsonObj.main.temp_max;
+  var city_pressure = jsonObj.main.pressure;
+  var city_humidity = jsonObj.main.humidity;
+  var city_wind_speed = jsonObj.wind.speed;
+  var city_wind_deg = jsonObj.wind.deg;
+
+  $("#headercs").text(city_name);
+  $("#wmain").text(city_weather);
+  $("#wdesc").text(city_weather_desc);
+  $("#wtemp").text("Temp: "+city_temp);
+  $("#wfeels").text("Feels like: "+city_feels);
+  $("#whigh").text("High: "+city_temp_max);
+  $("#wlow").text("Low: "+city_temp_min);
+  $("#wspeed").text("Speed: "+city_wind_speed);
+  $("#wdeg").text("Degrees: "+city_wind_deg);
+  $("#wpressure").text("Pressure: "+city_pressure);
+  $("#whumidity").text("Humidity: "+city_humidity);
+}
